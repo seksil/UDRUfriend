3c92e9e091f4716c7d53702812666a5272dc22aa
seksil <seksil.ma@udru.ac.th>
2018-11-19 16:38:14 +0700

    Finish 19 nov

--- app/src/main/java/th/ac/udru/seksil/RegisterFragment.java
+++ app/src/main/java/th/ac/udru/seksil/RegisterFragment.java
@@ -4,11 +4,28 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class RegisterFragment extends Fragment {
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+    }   // Main Method
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(R.string.register);
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(R.string.message_have_space);
+    }
 
     @Nullable
     @Override
