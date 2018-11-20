aa6db89bc80f9f244a022991a634c4b1e76d3b50
seksil <seksil.ma@udru.ac.th>
2018-11-20 10:33:29 +0700

    Init

--- app/src/main/java/th/ac/udru/seksil/RegisterFragment.java
+++ app/src/main/java/th/ac/udru/seksil/RegisterFragment.java
@@ -1,5 +1,6 @@
 package th.ac.udru.seksil;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class RegisterFragment extends Fragment {
 
@@ -18,7 +20,24 @@ public class RegisterFragment extends Fragment {
 //        Create Toolbar
         createToolbar();
 
+//        Avata Controller
+        avataController();
+
     }   // Main Method
+
+    private void avataController() {
+        ImageView imageView = getView().findViewById(R.id.imageViewAvatar);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Please Choose App and Image"), 5);
+
+            }
+        });
+    }
 
     private void createToolbar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
@@ -27,7 +46,15 @@ public class RegisterFragment extends Fragment {
         ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(R.string.message_have_space);
         ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
         ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
     }
+
 
     @Nullable
     @Override
