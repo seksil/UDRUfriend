dfe27211df616a54317b2c8e777b4f85ebbb3e72
seksil <seksil.ma@udru.ac.th>
2018-11-20 11:26:19 +0700

    Get Image OK

--- app/src/main/java/th/ac/udru/seksil/RegisterFragment.java
+++ app/src/main/java/th/ac/udru/seksil/RegisterFragment.java
@@ -1,6 +1,9 @@
 package th.ac.udru.seksil;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,8 +13,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class RegisterFragment extends Fragment {
+
+    //    Explicit
+    private Uri uri;
+    private ImageView imageView;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -25,8 +33,34 @@ public class RegisterFragment extends Fragment {
 
     }   // Main Method
 
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == getActivity().RESULT_OK) {
+
+            uri = data.getData();
+
+            try {
+
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
+                Bitmap bitmap1 = Bitmap.createScaledBitmap(bitmap, 800, 600, false);
+                imageView.setImageBitmap(bitmap1);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        } else {
+            Toast.makeText(getActivity(), "Please Choose Image", Toast.LENGTH_SHORT).show();
+        }
+
+    }   // Result
+
     private void avataController() {
-        ImageView imageView = getView().findViewById(R.id.imageViewAvatar);
+        imageView = getView().findViewById(R.id.imageViewAvatar);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
